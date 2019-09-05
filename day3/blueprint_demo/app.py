39eed8a9aaed583d85fc1c48198088d667051874
gaohj <gaohj@126.com>
2019-09-05 16:29:12 +0800

    flask_blueprint2

--- day3/blueprint_demo/app.py
+++ day3/blueprint_demo/app.py
@@ -7,6 +7,7 @@ app.config.update({
     'DEBUG':True,
     'TEMPLATES_AUTO_RELOAD':True
 })
+app.config['SERVER_NAME'] = 'jd.com:5001'
 app.register_blueprint(user_bp)
 app.register_blueprint(book_bp)
 app.register_blueprint(movie_bp)
--- day3/blueprint_demo/apps/movie.py
+++ day3/blueprint_demo/apps/movie.py
@@ -1,12 +1,13 @@
 #encoding:utf-8
-from flask import Blueprint
+from flask import Blueprint,render_template,url_for
 
-movie_bp = Blueprint('movie',__name__,url_prefix='/movie')
+# movie_bp = Blueprint('movie',__name__,url_prefix='/movie',static_folder="movie_static",template_folder="movie")
+movie_bp = Blueprint('movies',__name__,url_prefix='/movie',static_folder="movie_static")
 
 #http://127.0.0.1:5001/user/profile/
 @movie_bp.route("/list/")
 def list():
-    return '电影列表页面'
+    return render_template("movie_list.html")
 
 @movie_bp.route("/detail/")
 def detail():
