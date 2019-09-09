15db70443d81435a2ce51207b196bca3c129f071
gaohj <gaohj@126.com>
2019-09-09 16:13:23 +0800

    wtfform2

--- day6/flask_form_type/app.py
+++ day6/flask_form_type/app.py
@@ -1,8 +1,10 @@
 from flask import Flask,request,render_template
-from forms import RegisterForm
+from forms import RegisterForm,LoginForm
+from flask_bootstrap import Bootstrap
 app = Flask(__name__)
-
-
+app.config['SECRET_KEY'] = '123ADFASD112'
+app.config['BOOTSTRAP_SERVE_LOCAL']=True
+bootstrap = Bootstrap(app)
 
 @app.route('/')
 def hello_world():
@@ -10,9 +12,9 @@ def hello_world():
 
 @app.route('/register/',methods=['GET','POST'])
 def register():
-
+    form = RegisterForm()
     if request.method == "GET":
-        return render_template('register.html')
+        return render_template('register.html',form=form)
     else:
         # request.args.get()
         # request.POST.get()
@@ -25,6 +27,18 @@ def register():
             print(form.errors)  #打印表单错误消息
             return 'fail'
 
+@app.route('/login/',methods=['GET','POST'])
+def login():
+    form = LoginForm()
+    if request.method == "GET":
+        return render_template('login.html',form=form)
+    else:
+        form = LoginForm(request.form) # 表单提交多来的内容作为 参数
+        if form.validate(): #如果用户的输入 满足要求
+            return 'success'
+        else:
+            print(form.errors)  #打印表单错误消息
+            return 'fail'
 
 if __name__ == '__main__':
     app.run(debug=True,port=5001)
--- day6/flask_form_type/forms.py
+++ day6/flask_form_type/forms.py
@@ -1,10 +1,29 @@
-from wtforms import Form,StringField,PasswordField,SubmitField
-from wtforms.validators import Length,EqualTo
-
+from wtforms import Form,StringField,PasswordField,SubmitField,IntegerField,BooleanField,SelectField,DateField
+from wtforms.validators import Length,EqualTo,Email,InputRequired,NumberRange,Regexp,URL,UUID
+from flask_wtf import FlaskForm
 class RegisterForm(Form):
-    username = StringField(validators=[Length(min=6,max=20,message="用户名长度在6-20位")])
-    password = PasswordField(validators=[Length(min=6,max=20,message="密码长度在6-20位")])
-    password_repeat =  PasswordField(validators=[Length(min=6,max=20,message="密码长度在6-20位"),EqualTo("password",message="两次密码必须一致")])
+    username = StringField("用户名",validators=[Length(min=6,max=20,message="用户名长度在6-20位")])
+    password = PasswordField("密码",validators=[Length(min=6,max=20,message="密码长度在6-20位")])
+    password_repeat =  PasswordField("确认密码",validators=[Length(min=6,max=20,message="密码长度在6-20位"),EqualTo("password",message="两次密码必须一致")])
+    submit = SubmitField("立即注册")
+
+
+class LoginForm(FlaskForm):
+    email = StringField("邮箱",validators=[Email(message="必须是邮箱类型")])
+    username = StringField("用户名", validators=[Length(min=6, max=20, message="用户名长度在6-20位"),InputRequired("请填写用户名")])
+    age = IntegerField(validators=[NumberRange(18,90)])
+    phone = StringField(validators=[Regexp(r'1[3-9]\d{9}')])
+    homepage = StringField(validators=[URL()])
+    uuid = StringField(validators=[UUID()])
+    captcha = StringField(validators=[Length(4,4)])
+    creat_time = DateField("注册时间",validators=[])
+    remember = BooleanField("记住我:")
+    tags = SelectField("标签",choices=[('1','python'),('2','java')])
+    submit = SubmitField("立即登录")
+    #http://www.jiabin.com
+
+
+
 
 
 
